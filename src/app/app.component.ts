851c1d69c9ed4afbbf0bfceb454d6385fb36d27c
Nesma91 <nesmataha91@gmail.com>
2024-05-03 10:36:53 +0300

    Adding Shop Feature and Enhancing the Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +7,8 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http
-      .get<IPagination<IProduct[]>>(
-        'https://localhost:5001/api/products?pageSize=50'
-      )
-      .subscribe({
-        next: (response) => (this.products = response.data),
-        error: (error) => console.log(error),
-        complete: () => {
-          console.log('request completed');
-          console.log('extra statements...');
-        },
-      });
-  }
+  ngOnInit(): void {}
 }
---
+++ src/app/shop/shop.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/type';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(shopParams: ShopParams) {
+    let params = new HttpParams();
+
+    if (shopParams.brandId > 0)
+      params = params.append('brandId', shopParams.brandId);
+    if (shopParams.typeId) params = params.append('typeId', shopParams.typeId);
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
+    if (shopParams.search) params = params.append('search', shopParams.search);
+
+    return this.http.get<IPagination<IProduct[]>>(`${this.baseUrl}products`, {
+      params: params,
+    });
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(`${this.baseUrl}products/brands`);
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(`${this.baseUrl}products/types`);
+  }
+}
